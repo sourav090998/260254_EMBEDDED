503c3eb5ab0576380839746c2e354130411e5120
sourav090998 <64220562+sourav090998@users.noreply.github.com>
2021-04-29 04:38:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+/**
+ * @file main.c
+ * @author Sourav Sahoo
+ * @brief Project to design a heated seats for Automobiles
+ * @version 0.1
+ * @date 2021-04-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include<avr/io.h>
+#include"Activity1.h"      //Header file for Activity_1
+#include"Activity2.h"      //Header file for Activity_2
+#include"Activity3.h"      //Header file for Activity_3
+#include"Activity4.h"      //Header file for Activity_4
+
+volatile int LED_STATUS=0; //Variable storing LED status
+volatile int temp_out=0;   //Variable storing Temp Out
+uint16_t temp=0xFFFF;      //Variable storing Temp Input
+
+int main(void)             // Main Funclion
+{
+
+    while(1)               // Infinite Loop
+    {
+       LED_STATUS=Activity1(LED_STATUS);// Function for checking the GPIO Status
+       if(LED_STATUS==1)                // If LED_Status in "TRUE"
+       {
+           temp=Activity2();            //Function for Reading the Analog value from ADC Pin.
+           temp_out=Activity3(temp);    //Function for setting the PWM according to 'temp' value.
+           Activity4(temp_out);         //Send the tempout value using UART
+
+       }
+       else                             // If LED_Status in "FALSE"
+       {
+           temp=0x0000;                 // Initialize 'temp' to 0.
+           temp_out=0;                  // Initialize 'temp' to 0.
+           Activity4(temp_out);         //Send the tempout value using UART
+        }
+
+    }
+    return 0;
+}
